7e77fa7edcb9d0f8db9788a6a7a48661da4bbd43
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-09-27 12:31:34 +0100

    Create largeNO3.py

---
+++ largeNO3.py
@@ -0,0 +1,9 @@
+
+num3 = float(input("Enter third number: "))
+if (num1 > num2) and (num1 > num3):
+largest = num1
+elif (num2 > num1) and (num2 > num3):
+largest = num2
+else:
+largest = num3
+print("The  number is largest")
