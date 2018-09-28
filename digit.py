828e9ef316f8980981d3632fd83272ff2cf67f1b
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-09-28 17:38:53 +0530

    Create digit.py

---
+++ digit.py
@@ -0,0 +1,7 @@
+num=int(input("please Enter any number"))
+Count = 0
+while(num > 0):
+    num =num // 10
+    Count = Count + 1
+
+print("\n Number of Digits in a Given Number = %d" %Count)num=int(input("please Enter any number"))
