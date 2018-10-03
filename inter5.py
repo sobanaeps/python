42031ab40e6feb9f2c39fabd3a0cb663396297dc
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-10-03 17:53:23 +0530

    Create inter5.py

---
+++ inter5.py
@@ -0,0 +1,8 @@
+print(no's btwn", lower,"and", upper,"are:")
+for num i in range(lower,upper+1):
+    if num >1:
+         fori in range(2,num);
+          if(num%i)==0:
+              break:
+  else:
+       print(num)
