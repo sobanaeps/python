c7d396ae26cbdf303463d1961ad3816b6168f143
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-10-03 17:08:50 +0530

    Update armstrong2.py

--- armstrong2.py
+++ armstrong2.py
@@ -5,3 +5,10 @@ while temp > 0:
  temp//=10
    if (num ==sum):
      print (num)
+temp = num
+while temp > 0:
+    digit = temp%10
+ sum += digit**order
+ temp//=10
+   if (num ==sum):
+     print (num)
