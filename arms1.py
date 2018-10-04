d749d68c5e4976cece3929bfa7bb7fab0acb6274
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-10-04 16:32:26 +0530

    Create arms1.py

---
+++ arms1.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number"))
+for num in range(1,n)
+  num=0
+  temp=num
+  while temp >0:
+      digit=temp%10
+      num=num=(digit**3)
+      temp=temp?10
+     if num==sum:
+         print num
