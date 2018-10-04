34e9c90932dc0f3b054c1c7d76a07ff36e88bd1a
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-10-04 16:46:55 +0530

    Create progress.py

---
+++ progress.py
@@ -0,0 +1,8 @@
+def Nth_of_AP(a, d, N) : 
+    Nth term t(n) = a(1) + (n-1)*d 
+    return (a + (N - 1) * d) 
+a = 2   
+d = 1   
+N = 5 
+   print( "The ", N ,"th term of the series is : ", Nth_of_AP(a, d, N)) 
+  
