106b309d3c3cf2058573976d34e60dbbb3963d5d
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-12-12 14:02:01 +0530

    Create swapp.py

---
+++ swapp.py
@@ -0,0 +1,12 @@
+str1=input("Enter first string:");
+str2=input("Enter second string:");
+if str1=='x':
+      print("break")
+else:
+     temp=str1
+     str1=str2
+     str2=temp
+     print("string after swapping");
+     print("first string:",str1)
+     print("second string:",str2)
+ 
