d4bbb19016340f56b90c03d5048562457d75af2b
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2018-12-14 14:35:24 +0530

    Create charswap.py

---
+++ charswap.py
@@ -0,0 +1,10 @@
+x = input("Enter value of x: ")
+y = input("Enter value of y: ")
+if char=="x"
+x = 5
+y = 10
+temp = x
+x = y
+y = temp
+print("The value of x after swapping: ")
+print("The value of y after swapping: ")
