b5285d66a4ccf2c25b140603dff85a5c9d4eb2f1
sobanaeps <43606277+sobanaeps@users.noreply.github.com>
2019-02-11 14:49:48 +0530

    Create stringk.py

---
+++ stringk.py
@@ -0,0 +1,13 @@
+def findsubstrings(s,k):
+  int =[0]*26
+  for j in range(i,n):
+  int [ord(s[j])-ord('a')]+=1
+  if(int [ord(s[j])-ord('a')]<=k
+  ans+=1
+  else:
+ ans=0
+ n=len(s)
+  for i in range(n):
+  break
+  return ans
+ 
